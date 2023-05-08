915443c795072f9c04fdfd95b8975b59c356a007
Jupi <spacejupiter0@gmail.com>
2023-05-08 17:37:14 +0300

    added session controls

--- src/main/java/org/example/OOP/ActiveSession.java
+++ src/main/java/org/example/OOP/ActiveSession.java
@@ -1,12 +1,27 @@
 package org.example.OOP;
 
 public class ActiveSession {
-    public final String sessionId;
-    public final User user;
+    private String sessionId;
+    private User user;
 
     ActiveSession(String sessionId, User user)
     {
         this.sessionId = sessionId;
         this.user = user;
+    }
+
+    public String getSessionId()
+    {
+        return this.sessionId;
+    }
+
+    public User getSessionUser()
+    {
+        return this.user;
+    }
+
+    public void terminate()
+    {
+        sessionsControl.TerminateSession(this); // make the session kill itself
     }
 }
--- src/main/java/org/example/OOP/sessionsControl.java
+++ src/main/java/org/example/OOP/sessionsControl.java
@@ -9,17 +9,18 @@ public class sessionsControl {
     private static List<ActiveSession> activeSessionsList = new ArrayList<>();
     public static ActiveSession InitiateSession(User sessionOwner)
     {
-        ActiveSession newSession = new ActiveSession(UUID.randomUUID().toString(), sessionOwner);
+        String SessionId = UUID.randomUUID().toString();
+        ActiveSession newSession = new ActiveSession(SessionId, sessionOwner);
         return newSession;
     }
 
     public static boolean TerminateSession(String sessionId)
     {
-        return activeSessionsList.removeIf((element) -> element.sessionId == sessionId);
+        return activeSessionsList.removeIf((element) -> element.getSessionId() == sessionId);
     }
 
     public static boolean TerminateSession(ActiveSession session)
     {
-        return activeSessionsList.removeIf((element) -> element.sessionId == session.sessionId);
+        return activeSessionsList.removeIf((element) -> element.getSessionId() == session.getSessionId());
     }
 }
